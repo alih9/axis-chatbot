e008851a432bd432b1775205500533a6c0e2c003
MuhammadUMerRiaz <umerri4@gmail.com>
2021-11-06 00:07:04 +0500

    Testing on live

--- customer/src/index.js
+++ customer/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App'
 import ChatBox from './component/ChatBox';
 import io from "socket.io-client";
-const socket = io.connect('http://localhost:4000/customertest');
+// const socket = io.connect('http://localhost:4000/');
+// const socket = io.connect('/');
+const socket = io(process.env.REACT_APP_NODE_API);
 // import { BrowserRouter as  Router,Route, Switch } from "react-router-dom";
 
 // var socket = io({transports: ['websocket'], upgrade: false}).connect('/');
@@ -15,10 +17,8 @@ ReactDOM.render(
   //   </Switch>
   // </Router>,
 
- //<App/>, 
 
- <ChatBox  _id={3} socket={socket} />, 
-
+ <ChatBox  _id={localStorage.getItem("tenant_id")} socket={socket} />, 
 
   document.getElementById('root')
 );
